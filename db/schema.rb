0d89733f737e2df5727212341147ae4734e99160
Karthikan <km00776@surrey.ac.uk>
2018-11-23 15:33:20 +0000

    Fixed Models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123122511) do
+ActiveRecord::Schema.define(version: 20181123130121) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
